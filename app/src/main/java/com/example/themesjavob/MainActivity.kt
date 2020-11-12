2208748a9c0f3d2df03745ff0ec4df6d11ac90ab
Fourcade7 <ccm8d777>
2020-11-13 02:36:54 +0500

    My First Commit pzds

---
+++ app/src/main/java/com/example/themesjavob/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.themesjavob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
